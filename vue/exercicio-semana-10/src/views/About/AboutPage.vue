e884478e353a7c7a9a4c76fbd8731ba8a30d1748
Jusouza182 <juliana_s_souza@estudante.sesisenai.org.br>
2023-08-14 00:04:25 -0300

    atualiza instagram

---
+++ vue/exercicio-semana-10/src/views/About/AboutPage.vue
@@ -0,0 +1,10 @@
+<template>
+   <div>
+    <h1>Sobre</h1>
+    <p> Essa página é uma descrição do site e da empresa que ele pertece</p>
+   </div>
+   
+   <div>
+    <p>Aqui vai um mapa</p>
+   </div>
+</template>
--- vue/semana10/src/views/SignUp/SingUp.vue
+++ vue/semana10/src/views/SignUp/SingUp.vue
@@ -4,23 +4,32 @@
         <div class="form-element">
             <label for="name">Nome completo</label>
             <input class="form-input" id="name" v-model="name">
-        </div>
 
+            <span class="message-erro">{{ this.errors.name }}</span>
+        </div>
         <div class="form-element">
             <label for="email">E-mail</label>
             <input class="form-input" id="email" v-model="email">
+
+            <span class="message-erro">{{ this.errors.email }}</span>
         </div>
         <div class="form-element">
-            <label for="fone">Telefone</label>
-            <input class="form-input" id="fone" v-model="fone">
+            <label for="phone">Telefone</label>
+            <input class="form-input" id="phone" v-model="phone">
+
+            <span class="message-erro">{{ this.errors.phone }}</span>
         </div>
         <div class="form-element">
             <label for="password">Senha</label>
             <input class="form-input" type="password" id="password" v-model="password">
+
+            <span class="message-erro">{{ this.errors.password }}</span>
         </div>
         <div class="form-element">
             <label for="confirmPassword">Confirmar senha</label>
             <input class="form-input" type="password" id="confirmPassword" v-model="confirmPassword">
+
+            <span class="message-erro">{{ this.errors.confirmPassword }}</span>
         </div>
         <div class="form-element">
             <label for="sponsor">Patrocinador</label>
@@ -37,7 +46,7 @@
         </textarea>
         </div>
         <div>
-        <p>Selecione um tipo de plano</p>
+            <p>Selecione um tipo de plano</p>
             <input type="radio" value="1" v-model="planType"> <label>Bronze</label>
             <input type="radio" value="2" v-model="planType"> <label>Prata</label>
             <input type="radio" value="3" v-model="planType"> <label>Premium</label>
@@ -46,31 +55,71 @@
             <label for="confirmTerm">
                 <input type="checkbox" id="confirmTerm" v-model="confirmTerm"> Aceita termos de uso
             </label>
+            <span class="message-erro">{{ this.errors.confirmTerm }}</span>
+
         </div>
         <button type="submit">Criar conta</button>
     </form>
 </template>
 <script>
+
+import * as yup from 'yup'
+import { captureErrorYup } from '../../utils/captureErrorYup'
+
 export default {
     data() {
         return {
             name: '',
             email: '',
-            fone: '',
+            phone: '',
             password: '',
             confirmPassword: '',
             sponsor: '',
             bio: '',
             confirmTerm: true,
-            planType: '2'
+            planType: '2',
+
+            errors: {}
         }
     },
     methods: {
-    handleCreateAccount() {
+        handleCreateAccount() {
+            try {
+                // 1 -  Validar dados
+                const schema = yup.object().shape({
+                    name: yup.string().required("Nome é obrigatório"),
+                    email: yup.string().email("E-mail não é válido").required("Email é obrigatório"),
+                    phone: yup.string().required("Telefone é obrigatório"),
+                    password: yup.string().min(8, 'A senha deve ser maior').max(20, 'Deve ter entre 8-20 letras').required('A senha é obrigatória'),
+                    confirmPassword: yup.string().required('A confirmação é necessária').oneOf([yup.ref('password')], 'As senhas devem coincidir'),
+                    confirmTerm: yup.boolean().isTrue('O termo de uso deve ser aceito')
+                })
 
-    }}
+                schema.validateSync(
+                    {
+                        name: this.name,
+                        email: this.email,
+                        phone: this.phone,
+                        password: this.password,
+                        verifyPassword: this.confirmPassword,
+                        confirmTerm: this.confirmTerm
+                    },
+                    // Faz com que seja processado todos os campos de uma vez
+                    { abortEarly: false }
+                )
+
+
+
+            } catch (error) {
+                console.log(error)
+                if (error instanceof yup.ValidationError) {
+                    this.errors = captureErrorYup(error)
+                }
+
+            }
+        }
+    }
 }
-
 
 </script>
 <style scoped>
@@ -106,10 +155,12 @@ export default {
     outline: none;
 
 }
-.form-select{
+
+.form-select {
     width: 102%;
 }
-.form-textarea{
+
+.form-textarea {
     height: 50px;
 }
 
@@ -127,6 +178,12 @@ button {
 button:hover {
     background-color: #286ee0;
 }
+
+.message-erro {
+    color: red;
+    margin: 4px;
+}
+
 .input-error {
     border-color: red;
 }
